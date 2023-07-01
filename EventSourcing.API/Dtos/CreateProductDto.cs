89d729ca04869c114296781102050557ff23fe8f
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-01 20:20:11 +0300

    Dtos have been created.

---
+++ EventSourcing.API/Dtos/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EventSourcing.API.Dtos
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int UserId { get; set; }
+    }
+}
