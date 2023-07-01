80b6aa80a91ae92a82910c39ef42886c88ffa4f8
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-01 22:59:37 +0300

    ProductsController has been added.

---
+++ EventSourcing.API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using EventSourcing.API.Dtos;
+using EventSourcing.API.Features.Commands.Products.ChangeProductName;
+using EventSourcing.API.Features.Commands.Products.ChangeProductPrice;
+using EventSourcing.API.Features.Commands.Products.CreateProduct;
+using EventSourcing.API.Features.Commands.Products.DeleteProduct;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcing.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto createProductDto)
+        {
+            await _mediator.Send(new CreateProductCommand { CreateProductDto = createProductDto });
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChaneName(ChangeProductNameDto changeProductNameDto)
+        {
+            await _mediator.Send(new ChangeProductNameCommand { ChangeProductNameDto = changeProductNameDto });
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChanePrice(ChangeProductPriceDto changeProductPriceDto)
+        {
+            await _mediator.Send(new ChangeProductPriceCommand { ChangeProductPriceDto = changeProductPriceDto });
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteProductCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
