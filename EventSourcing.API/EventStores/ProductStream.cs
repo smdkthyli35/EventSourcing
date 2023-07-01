d44d8cc8f9a8004032078bcc8f234a4fc8603abd
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-01 22:43:16 +0300

    ProductStream has been added.

---
+++ EventSourcing.API/EventStores/ProductStream.cs
@@ -0,0 +1,53 @@
+﻿using EventSourcing.API.Dtos;
+using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+
+namespace EventSourcing.API.EventStores
+{
+    public class ProductStream : AbstractStream
+    {
+        public static string StreamName => "ProductStream";
+
+        public ProductStream(IEventStoreConnection eventStoreConnection) : base(StreamName, eventStoreConnection)
+        {
+        }
+
+        public void Created(CreateProductDto createProductDto)
+        {
+            Events.AddLast(new ProductCreatedEvent
+            {
+                Id = Guid.NewGuid(),
+                Name = createProductDto.Name,
+                Price = createProductDto.Price,
+                Stock = createProductDto.Stock,
+                UserId = createProductDto.UserId
+            });
+        }
+
+        public void NameChanged(ChangeProductNameDto changeProductNameDto)
+        {
+            Events.AddLast(new ProductNameChangedEvent
+            {
+                Id = changeProductNameDto.Id,
+                ChangedName = changeProductNameDto.Name
+            });
+        }
+
+        public void PriceChanged(ChangeProductPriceDto changeProductPriceDto)
+        {
+            Events.AddLast(new ProductPriceChangedEvent
+            {
+                Id = changeProductPriceDto.Id,
+                ChangedPrice = changeProductPriceDto.Price
+            });
+        }
+
+        public void Deleted(Guid id)
+        {
+            Events.AddLast(new ProductDeletedEvent
+            {
+                Id = id
+            });
+        }
+    }
+}
--- EventSourcing.API/Program.cs
+++ EventSourcing.API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
 builder.Services.AddEventStore(builder.Configuration);
+builder.Services.AddSingleton<ProductStream>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
